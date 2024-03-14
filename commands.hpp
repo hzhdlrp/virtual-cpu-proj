3a0cc0a5fc0146cb11b34bf06c0133a7072aa8fb
hzhdlrp <pswjft@gmail.com>
2024-03-15 02:23:13 +0300

    .

--- commands.hpp
+++ commands.hpp
@@ -1,3 +1,5 @@
+#include <utility>
+
 #ifndef PROJECT2_COMMANDS_H
 #define PROJECT2_COMMANDS_H
 
@@ -14,13 +16,9 @@ struct Registers {
 
 struct Commands {
     Commands() = default;
-    Commands(const Commands&) = delete;
-    Commands(Commands&&) = delete;
-    Commands& operator=(const Commands&) = delete;
-    Commands& operator=(Commands&&) = delete;
-
-    virtual void doit() = 0;
-    std::ifstream *_inputFile = nullptr;
+    virtual void setReg(std::ifstream *input) {}
+    virtual void setValue(std::ifstream *input) {}
+    virtual void doit() {};
 };
 
 struct Begin : Commands {
@@ -36,17 +34,15 @@ struct Push : Commands {
     size_t _value = 0;
     Stack<size_t> *_stack = nullptr;
 
-    Push(std::ifstream *input, Stack<size_t> *stack) {
-        _inputFile = input;
+    void setValue(std::ifstream *input) override {
+        *input >> _value;
+    }
+
+    explicit Push(Stack<size_t> *stack) {
         _stack = stack;
     }
 
     void doit() override {
-        if (_inputFile->is_open())
-            (*_inputFile) >> _value;
-        else
-            throw std::runtime_error("can't read a file");
-
         _stack->push(_value);
     }
 };
@@ -55,18 +51,16 @@ struct Pushr : Push {
     std::string _reg = std::string("");
     Registers *_registers = nullptr;
 
-    Pushr(std::ifstream *input, Stack<size_t> *stack, Registers *registers) : Push(input, stack) {
-        _inputFile = input;
+    void setReg(std::ifstream *input) override {
+        *input >> _reg;
+    }
+
+    Pushr(Stack<size_t> *stack, Registers *registers) : Push(stack) {
         _stack = stack;
         _registers = registers;
     }
 
     void doit() override {
-        if (_inputFile->is_open())
-            (*_inputFile) >> _reg;
-        else
-            throw std::runtime_error("can't read a file");
-
         if (_reg == "AX") _stack->push(_registers->AX);
         if (_reg == "BX") _stack->push(_registers->BX);
         if (_reg == "CX") _stack->push(_registers->CX);
@@ -90,18 +84,16 @@ struct Popr : Pop {
     std::string _reg = std::string("");
     Registers *_registers = nullptr;
 
-    Popr(std::ifstream *input, Stack<size_t> *stack, Registers *registers) : Pop(stack) {
-        _inputFile = input;
+    void setReg(std::ifstream *input) override {
+        *input >> _reg;
+    }
+
+    Popr(Stack<size_t> *stack, Registers *registers): Pop(stack) {
         _stack = stack;
         _registers = registers;
     }
 
     void doit() override {
-        if (_inputFile->is_open())
-            (*_inputFile) >> _reg;
-        else
-            throw std::runtime_error("can't read a file");
-
         if (_reg == "AX") _stack->push(_registers->AX);
         if (_reg == "BX") _stack->push(_registers->BX);
         if (_reg == "CX") _stack->push(_registers->CX);
@@ -179,9 +171,7 @@ struct Div : Commands {
 };
 
 struct Out : Pop {
-    explicit Out(Stack<size_t> *stack) : Pop(stack) {
-            _stack = stack;
-    }
+    explicit Out(Stack<size_t> *stack) : Pop(stack) {}
 
     void doit() override {
         std::cout << _stack->top() << std::endl;
@@ -190,15 +180,11 @@ struct Out : Pop {
 };
 
 struct In : Push {
-    In(std::ifstream *input, Stack<size_t> *stack) : Push(input, stack) {
-        _inputFile = input;
-        _stack = stack;
-    }
+    explicit In(Stack<size_t> *stack) : Push(stack) {}
 
     void doit() override {
         std::cin >> _value;
         _stack->push(_value);
     }
 };
-
 
--- main.cpp
+++ main.cpp
@@ -9,12 +9,21 @@
 class Virtual {
 public:
     void start() {
-        std::string cmd = std::string("");
         do {
-            (*_inputFile) >> cmd;
-            if (_commandsByNames.contains(cmd))
-//                std::cout << cmd << std::endl;
-                _commandsByNames[cmd]->doit();
+            std::string cmd = std::string("");
+            *_inputFile >> cmd;
+            std::unique_ptr<Commands> command = std::make_unique<Commands>();
+            *command = *_commandsByNames[cmd];
+            command->setReg(_inputFile);
+            command->setValue(_inputFile);
+            _commandsVector.push_back(command);
+            if (cmd == std::string("BEGIN")) _isBegun = true;
+
+            if (_isBegun) {
+                if (_commandsByNames.contains(cmd)) {
+                    _commandsByNames[cmd]->doit();
+                }
+            }
         } while (cmd != std::string("END"));
     }
 
@@ -23,28 +32,34 @@ public:
         _inputFile = input;
         _commandsByNames["BEGIN"] = std::make_unique<Begin>();
         _commandsByNames["END"] = std::make_unique<End>();
-        _commandsByNames["PUSH"] = std::make_unique<Push>(_inputFile, &_stack);
-        _commandsByNames["PUSHR"] = std::make_unique<Pushr>(_inputFile, &_stack, &_registers);
+        _commandsByNames["PUSH"] = std::make_unique<Push>(&_stack);
+        _commandsByNames["PUSHR"] = std::make_unique<Pushr>(&_stack, &_registers);
         _commandsByNames["POP"] = std::make_unique<Pop>(&_stack);
-        _commandsByNames["POPR"] = std::make_unique<Popr>(_inputFile, &_stack, &_registers);
+        _commandsByNames["POPR"] = std::make_unique<Popr>(&_stack, &_registers);
         _commandsByNames["ADD"] = std::make_unique<Add>(&_stack, &_registers);
         _commandsByNames["SUB"] = std::make_unique<Sub>(&_stack, &_registers);
         _commandsByNames["MUL"] = std::make_unique<Mul>(&_stack, &_registers);
         _commandsByNames["DIV"] = std::make_unique<Div>(&_stack, &_registers);
         _commandsByNames["OUT"] = std::make_unique<Out>(&_stack);
-        _commandsByNames["IN"] = std::make_unique<In>(_inputFile, &_stack);
+        _commandsByNames["IN"] = std::make_unique<In>( &_stack);
     }
 private:
     std::ifstream *_inputFile = nullptr;
     Stack<size_t> _stack;
     Registers _registers;
-    std::vector<std::string> _commandsNamesStack;
-
+    std::vector<std::unique_ptr<Commands>> _commandsVector;
+    std::map<std::string , int> _labelsIndexes;
+//    std::vector<int> _functionsIndexes;
+//    bool _inFunction = false;
+    bool _isBegun = false;
     std::map<std::string, std::unique_ptr<Commands>> _commandsByNames;
 };
 
 int main() {
-    std::ifstream input("/Users/vadimleonov/Desktop/алгосы/virtual-cpu-tests/fibo");
+    std::string inputFile;
+    std::cin >> inputFile;
+    //  /Users/vadimleonov/Desktop/алгосы/virtual-cpu-tests/fibo
+    std::ifstream input(inputFile);
     Virtual myCpu(&input);
     myCpu.start();
     return 0;
