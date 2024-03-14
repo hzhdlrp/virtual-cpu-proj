b4ad1e33796d180771a14b3545d988539aee7ba4
hzhdlrp <pswjft@gmail.com>
2024-03-14 19:04:43 +0300

    .

--- commands.hpp
+++ commands.hpp
@@ -8,12 +8,13 @@ struct Registers {
     size_t BX = 0;
     size_t CX = 0;
     size_t DX = 0;
+    size_t F1X = 0;
+    size_t F2X = 0;
 };
 
 struct Commands {
     virtual void doit() = 0;
     std::ifstream *_inputFile = nullptr;
-    std::ifstream *_outputFile = nullptr;
 };
 
 struct Begin : Commands {
@@ -100,11 +101,97 @@ struct Popr : Pop {
         if (_reg == "DX") _stack->push(_registers->DX);
         _stack->pop();
     }
-
-
-
 };
 
+struct Add : Commands {
+    Stack<size_t> *_stack;
+    Registers *_registers;
 
+    Add(Stack<size_t> *stack, Registers *registers) {
+        _stack = stack;
+        _registers = registers;
+    }
+
+    void doit() override {
+        _registers->F1X = _stack->top();
+        _stack->pop();
+        _registers->F2X = _stack->top();
+        _stack->push(_registers->F1X + _registers->F2X);
+    }
+};
+
+struct Sub : Commands {
+    Stack<size_t> *_stack;
+    Registers *_registers;
+
+    Sub(Stack<size_t> *stack, Registers *registers) {
+        _stack = stack;
+        _registers = registers;
+    }
+
+    void doit() override {
+        _registers->F1X = _stack->top();
+        _stack->pop();
+        _registers->F2X = _stack->top();
+        _stack->push(_registers->F1X - _registers->F2X);
+    }
+};
+
+struct Mul : Commands {
+    Stack<size_t> *_stack;
+    Registers *_registers;
+
+    Mul(Stack<size_t> *stack, Registers *registers) {
+        _stack = stack;
+        _registers = registers;
+    }
+
+    void doit() override {
+        _registers->F1X = _stack->top();
+        _stack->pop();
+        _registers->F2X = _stack->top();
+        _stack->push(_registers->F1X * _registers->F2X);
+    }
+};
+
+struct Div : Commands {
+    Stack<size_t> *_stack;
+    Registers *_registers;
+
+    Div(Stack<size_t> *stack, Registers *registers) {
+        _stack = stack;
+        _registers = registers;
+    }
+
+    void doit() override {
+        _registers->F1X = _stack->top();
+        _stack->pop();
+        _registers->F2X = _stack->top();
+        _stack->push(_registers->F1X / _registers->F2X);
+    }
+};
+
+struct Out : Pop {
+    explicit Out(Stack<size_t> *stack) : Pop(stack) {
+            _stack = stack;
+    }
+
+    void doit() override {
+        std::cout << _stack->top();
+        _stack->pop();
+    }
+};
+
+struct In : Push {
+    In(std::ifstream *input, Stack<size_t> *stack) : Push(input, stack) {
+        _inputFile = input;
+        _stack = stack;
+    }
+
+    void doit() override {
+        std::cin >> _value;
+        _stack->push(_value);
+    }
+};
 
 
--- main.cpp
+++ main.cpp
@@ -9,36 +9,42 @@
 
 class Virtual {
 public:
-
-    void setInputFile(std::ifstream *input) {
-        _inputFile = input;
-    }
-    void setOutputFile(std::ofstream *output) {
-        _outputFile = output;
-    }
-
-    void start(std::ifstream *input, std::ofstream *output) {
+    void start(std::ifstream *input) {
         setInputFile(input);
-        setOutputFile(output);
-
+        std::string cmd;
+        do {
+            (*_inputFile) >> cmd;
+            _commandsByNames[cmd]->doit();
+        } while (cmd != std::string("END"));
     }
 private:
     std::ifstream *_inputFile = nullptr;
-    std::ofstream *_outputFile = nullptr;
-    bool _begin = false;
-
     Stack<size_t> _stack;
-    Registers registers;
+    Registers _registers;
     std::vector<std::string> _commandsNamesStack;
+
     std::map<std::string, std::unique_ptr<Commands>> _commandsByNames = {
             {"BEGIN", std::make_unique<Begin>()},
             {"END", std::make_unique<End>()},
             {"PUSH", std::make_unique<Push>(_inputFile, &_stack)},
-            {"PUSHR", std::make_unique<Pushr>(_inputFile, &_stack, &registers)},
+            {"PUSHR", std::make_unique<Pushr>(_inputFile, &_stack, &_registers)},
             {"POP", std::make_unique<Pop>(&_stack)},
-
+            {"POPR", std::make_unique<Popr>(_inputFile, &_stack, &_registers)},
+            {"ADD", std::make_unique<Add>(&_stack, &_registers)},
+            {"SUB", std::make_unique<Sub>(&_stack, &_registers)},
+            {"MUL", std::make_unique<Mul>(&_stack, &_registers)},
+            {"DIV", std::make_unique<Div>(&_stack, &_registers)},
+            {"OUT", std::make_unique<Out>(&_stack)},
+            {"IN", std::make_unique<In>(_inputFile, &_stack)},
     };
+
+    void setInputFile(std::ifstream *input) {
+        _inputFile = input;
+    }
 };
+
 int main() {
+    Virtual myCpu;
+    myCpu.start("");
     return 0;
 }
